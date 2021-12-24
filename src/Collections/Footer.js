9993dc76a4cdf226e99bd7cebdf9832c08068560
Your Name <you@example.com>
2021-12-24 05:20:51 -0800

    Day1 code

---
+++ src/Collections/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Style.css";
+ function Footer() {
+    return (
+        <div id='Footer-css'>
+           <h1>Footer</h1> 
+        </div>
+    )
+}
+export default Footer;
