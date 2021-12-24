3a0fb052cdd9194936866c37270fd1ce54dea46f
Your Name <you@example.com>
2021-12-24 05:42:50 -0800

    Day1 code

---
+++ src/Collections/Users.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ function Users() {
+    return (
+        <div>
+            <center>
+            <h1>Users List</h1>
+            </center>
+        </div>
+    )
+}
+export default Users;
